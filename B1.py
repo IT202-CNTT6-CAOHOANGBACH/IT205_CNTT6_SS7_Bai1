890281ffbcfff1b2553c4fb59754e00b6ca6eeb7
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2026-06-02 22:12:12 +0700

    l2

--- B1.py
+++ B1.py
@@ -8,3 +8,11 @@ email_reset = email.lower().strip()
 print("Họ tên:", student_name_reset)
 print("Mã học viên:", student_code_reset)
 print("Email:", email_reset)
+
+# Vì sao student_name.strip() không làm thay đổi trực tiếp biến student_name?
+# Vì sao student_name.title() không tạo ra kết quả "Nguyen Van A" trong chương trình hiện tại?
+# Vì sao student_code.upper() không làm mã học viên chuyển thành chữ hoa?
+# Vì sao email.lower() không làm email chuyển thành chữ thường?
+# Muốn các phương thức xử lý chuỗi có hiệu lực, cần làm gì?
+# đáp án
+# vì nó chưa dc gán giá trị mới vậy nên cần gán giá trị mới sau khi sự dụng
